1087313370a26ea910f0e80478b52aeb02ca6a2f
caleblee24 <caleblee@gmail.com>
2025-04-11 14:12:11 -0700

    Delete backend directory

--- backend/routes/heatmapRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getHeatmapData } = require('../controllers/heatmapController');
-
-router.get('/', getHeatmapData);
-
-module.exports = router;
